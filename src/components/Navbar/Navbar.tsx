29692d036fbde783178e81e85fe84bafff988c24
Grzesiek <kiebasag@gmail.com>
2023-02-20 18:04:12 +0100

    working on nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 
 
 
 const Navbar:React.FC = () => {   
-    return <div>Navbar</div>;
+    return (
+        //Korzystając z Flex chakry można wpisac dowolna własciwosc 
+        <Flex bg="white" height='44px' padding="6px 12px">
+            <Flex>
+                <Image src="/public/images/redditFace.svg" height="30px" />
+                <Image src="/public/images/redditText.svg" height="30px" />
+            </Flex>
+        </Flex>
+    );
 };
 export default Navbar;
